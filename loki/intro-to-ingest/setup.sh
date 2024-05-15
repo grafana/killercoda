4ecb02772b8505d6b553f9ea4a9289f773784e5a
Jayclifford345 <jay.clifford@grafana.com>
2024-05-15 14:24:00 +0100

    fix: script

--- loki/intro-to-ingest/setup.sh
+++ loki/intro-to-ingest/setup.sh
@@ -14,5 +14,4 @@ echo "Building training instance...."
 
 # Update and install required packages
 echo "Updating and installing required packages..."
-sudo apt-get update && apt install gpg -y; sudo mkdir -p /etc/apt/keyrings/; wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null; echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list; sudo apt-get update && sudo apt-get install alloy
-sudo apt-get install -y figlet; clear; echo -e "\e[32m$(figlet -f standard 'Intro to')\e[0m"; echo -e "\e[33m$(figlet -f standard 'Ingesting')\e[0m"
+sudo apt-get update && apt install gpg -y; sudo mkdir -p /etc/apt/keyrings/; wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null; echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list; sudo apt-get update && sudo apt-get install alloy; sudo apt-get install -y figlet; clear; echo -e "\e[32m$(figlet -f standard 'Intro to')\e[0m"; echo -e "\e[33m$(figlet -f standard 'Ingesting')\e[0m"
