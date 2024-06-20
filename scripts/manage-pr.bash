72660e5f5aa05178b3af801a68cfdd506da5abe4
Jack Baldry <jack.baldry@grafana.com>
2024-06-20 11:55:30 +0100

    Add workflow to regenerate tutorials
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

---
+++ scripts/manage-pr.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+readonly BRANCH="${BRANCH:-update-generated-tutorials}"
+readonly SUBJECT="${SUBJECT:-Update generated tutorials}"
+
+# commit adds and commits the updated files as the Grafanabot GitHub user.
+function commit {
+  git add .
+  git config --local user.email bot@grafana.com
+  git config --local user.name grafanabot
+  git commit --message "${SUBJECT}"
+}
+
+PR_STATUS=$(gh pr view "${BRANCH}" --json state --jq .state || true)
+readonly PR_STATUS
+
+if [[ "${PR_STATUS}" == OPEN ]]; then
+  gh pr checkout "${BRANCH}"
+
+  if ! git diff --exit-code; then
+    commit
+    git push
+  fi
+
+  exit 0
+fi
+
+# Remove the remote branch if it exists because there is no PR associated with it.
+if git fetch origin "${BRANCH}"; then
+  git push origin --delete "${BRANCH}"
+fi
+
+git checkout -b "${BRANCH}"
+
+if ! git diff --exit-code; then
+  commit
+  git push origin "refs/heads/${BRANCH}"
+  gh pr create --title "${SUBJECT}" --base main --head "${BRANCH}"
+fi
