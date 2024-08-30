6a10e22ae248961446a4e84a4acacf4fb37bd9e5
Jayclifford345 <jay.clifford@grafana.com>
2024-08-30 13:23:58 +0100

    updated script

--- tempo/quick-start/setup.sh
+++ tempo/quick-start/setup.sh
@@ -6,5 +6,4 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-compose-plugin
-clear
+sudo apt-get install -y docker-compose-plugin && clear && echo "Setup complete. You may now begin the tutorial."
