be317a2ff729db8ec4e08ebadb78f2728a60c764
Jayclifford345 <jay.clifford@grafana.com>
2024-09-04 15:50:43 +0100

    test background

--- tempo/quick-start/docker-compose-update.sh
+++ tempo/quick-start/docker-compose-update.sh
@@ -16,6 +16,5 @@ readonly ARCH VERSION_CODENAME
 printf 'deb [arch=%s signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu %s stable' "${ARCH}" "${VERSION_CODENAME}" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y docker-compose-plugin
-
+sudo apt-get install -y docker-compose-plugin 
 echo "Setup complete. You may now begin the tutorial."
