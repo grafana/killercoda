d480ba8b2cb2067929cbc84d2ce646ed8675bd8a
Jayclifford345 <jay.clifford@grafana.com>
2024-11-27 12:30:07 +0000

    added setup

---
+++ loki/logcli-tutorial/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -euf
+# shellcheck disable=SC3040
+(set -o pipefail 2> /dev/null) && set -o pipefail
+
+
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+ARCH="$(dpkg --print-architecture)"
+VERSION_CODENAME="$(source /etc/os-release && echo "${VERSION_CODENAME}")"
+readonly ARCH VERSION_CODENAME
+
+printf 'deb [arch=%s signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu %s stable' "${ARCH}" "${VERSION_CODENAME}" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+wget https://github.com/grafana/loki/releases/download/v3.3.0/logcli_3.3.0_amd64.deb && \
+sudo dpkg -i logcli_3.3.0_amd64.deb && \
+sudo apt-get install -y docker-compose-plugin && clear && echo "Setup complete. You may now begin the tutorial."
